960275d6e59e7c0f95e559142bb053d65867c980
mufaroxyz <81554673+mufaroxyz@users.noreply.github.com>
2024-10-04 00:38:36 +0200

    feat: Initial layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      container: {
+        center: true
+      },
+
+      colors: {
+        background: "#1e1f21",
+        theme: {
+          foreground: {
+            DEFAULT: '#ded2e8',
+            secondary: "#9e89b8"
+          },
+        },
+        text: {
+          foreground: "#d4d4d4",
+        }
+      }
+    },
+  },
+  plugins: [],
 }
