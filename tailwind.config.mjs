800f4ff6f02e544d5cdeecc1828de75070cae8fc
mufaroxyz <81554673+mufaroxyz@users.noreply.github.com>
2024-10-05 01:11:29 +0200

    feat: Add og meta tags

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       container: {
-        center: true
+        center: true,
       },
 
       colors: {
         background: "#1e1f21",
         theme: {
           foreground: {
-            DEFAULT: '#ded2e8',
-            secondary: "#9e89b8"
+            DEFAULT: "#ded2e8",
+            secondary: "#9e89b8",
           },
         },
         text: {
           foreground: "#d4d4d4",
-        }
-      }
+        },
+      },
     },
   },
   plugins: [],
-}
+};
