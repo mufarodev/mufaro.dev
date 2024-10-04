6ca0864ac20b36610a55f52121dc8de446d0b886
mufaroxyz <81554673+mufaroxyz@users.noreply.github.com>
2024-10-05 00:51:42 +0200

    feat: Add page content + layout change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare()
+  output: "server",
+  adapter: cloudflare(),
 });
