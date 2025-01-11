2d65f6a1fbc071aa771bc4b0b6d6ea954371fd58
mufaroxyz <mufaro.priv@gmail.com>
2025-01-11 13:38:28 +0100

    test: remove react from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   output: "server",
   adapter: cloudflare(),
 });
